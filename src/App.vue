81adb2d74e873d6c3337c75fdc7f896d2d06c6e4
gabriel.bucalon.dex <gabriel.bucalon@dextra-sw.com>
2021-05-28 17:07:07 -0300

    feat: stylization of the card component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>teste</div>
+  <article></article>
 </template>
 
 <script>
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <!-- Image -->
+    <!-- Description -->
+    <!-- Footer -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+};
+</script>
+
+<style lang="scss">
+.card {
+  background: #fff;
+  border-radius: 2px;
+  display: inline-block;
+  height: 300px;
+  margin: 1rem;
+  position: relative;
+  width: 300px;
+}
+
+.card {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+
+.card:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+</style>
+
