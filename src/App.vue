b3c3ea3f2bd872d217e3e8b73da4dd4bd1c2127d
gabriel.bucalon.dex <gabriel.bucalon@dextra-sw.com>
2021-05-28 17:13:30 -0300

    feat: stylization of the card component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <article></article>
+  <article>
+    <card></card>
+  </article>
 </template>
 
 <script>
+import Card from "./components/Card";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Card,
+  },
 };
 </script>
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,7 +12,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="css">
 .card {
   background: #fff;
   border-radius: 2px;
